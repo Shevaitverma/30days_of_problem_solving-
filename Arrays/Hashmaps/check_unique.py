494fd0b0f0a8f52b164dd4d8443f576c0d4c2b69
Shevaitverma <shevaitverma333@gmail.com>
2024-10-26 13:08:30 +0530

    adding a new approach in previous solution

--- Arrays/Hashmaps/check_unique.py
+++ Arrays/Hashmaps/check_unique.py
@@ -1,5 +1,22 @@
 # check unique element using hashset
 
+########### brute force ##
+# def solve(arr):
+#     unique = []
+#     i = 1
+#     while i < len(arr):
+#         if arr[i-1] == arr[i]:
+#             i += 1
+#         else:
+#             unique.append(arr[i-1])
+#             i += 1
+#     unique.append(arr[-1])
+
+#     #     if i not in unique:
+#     #         unique.append(i)
+#     return unique
+
+########## optimised  #####
 def unique(arr):
     unique_ele = set(arr)
     return unique_ele 
--- playground.py
+++ playground.py
@@ -1,47 +1,19 @@
-#####   3 sums:  in an array find all subarrays whose sum is 0
-
 def solve(arr):
-    n = len(arr)
-    result = []
+    unique = []
+    i = 1
+    while i < len(arr):
+        if arr[i-1] == arr[i]:
+            i += 1
+        else:
+            unique.append(arr[i-1])
+            i += 1
+    unique.append(arr[-1])
 
-    def two_sum(arr, target, i, j):
-        while i < j:
-            if arr[i] + arr[j] > target:
-                j -= 1
-            elif arr[i] + arr[j] < target:
-                i += 1
-            else:
-                # Found a triplet
-                result.append([-target, arr[i], arr[j]])
-                
-                # Move to the next different element
-                while i < j and arr[i] == arr[i + 1]:
-                    i += 1
-                while i < j and arr[j] == arr[j - 1]:
-                    j -= 1
-                
-                i += 1
-                j -= 1
+    #     if i not in unique:
+    #         unique.append(i)
+    return unique
 
-    if n < 3:
-        return []
+arr = [1,2,3,3,4,4,5,5,5,5,6]
 
-    # Sort the array
-    arr.sort()
-
-    # Fix one element
-    for i in range(n - 2):
-        if i > 0 and arr[i] == arr[i - 1]:
-            continue  # Skip duplicates
-
-        # Set the target for two_sum
-        n1 = arr[i]
-        target = -n1
-
-        two_sum(arr, target, i + 1, n - 1)
-
-    return result
-
-arr = [-1, 0, 1, 2, -1, -4]
 res = solve(arr)
 print(res)
