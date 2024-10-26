5739051cd95d0bdb4ef3c0706f9dadee78fdeff2
Shevaitverma <shevaitverma333@gmail.com>
2024-10-26 12:42:43 +0530

    fixed issue in one old array problem code

--- Arrays/Hashmaps/check_freq_v2.py
+++ Arrays/Hashmaps/check_freq_v2.py
@@ -11,17 +11,18 @@ def freq(arr, k):
     max_ele = max(arr)
     # print(max_ele)
     # precompute the hash
-    hash_map = [0]*max_ele
-    # print(hash_map)
-    for i in range(len(arr)-1):
+    hash_map = [0]*(max_ele+1)
+    print(len(hash_map))
+    for i in range(len(arr)):
         hash_map[arr[i]] += 1
-    # print(hash_map)
+    print(hash_map)
     # # to check the freq of K 
     return hash_map[k]
 
 
-arr = [1,2,3,4,4,5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,7,8,9,9,9,9,9,9,14,45,76]
+arr = [1,2,3,4,4,5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,7,8,9,9,9,9,9,9,14,45,76,100]
+# arr = [1,2,3,3,4,5,6,7,7,8]
 
-result = freq(arr, 6)
+result = freq(arr, 76)
 
 print(result)
